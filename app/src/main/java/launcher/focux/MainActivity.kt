b4ba6da522f83ae113830a0aaffa704d184bf937
indraamani <indraamani24@gmail.com>
2026-07-11 04:13:12 +0530

    add: Main Activity

--- app/src/main/java/launcher/focux/MainActivity.kt
+++ app/src/main/java/launcher/focux/MainActivity.kt
@@ -4,8 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import launcher.focux.ui.theme.FocuxTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,5 +38,54 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun MainScreen() {
+    val pinnedAppList: List<String> = listOf(
+        "Instagram",
+        "Whatsapp",
+        "Phone",
+        "Gpay"
+    )
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround,
+        modifier = Modifier
+            .fillMaxSize()
+            //.padding(top = 110.dp, bottom = 40.dp)
+            .combinedClickable(
+                onLongClick = {
 
+                },
+                onClick = {}
+            )
+    ) {
+        // Mock Widget
+        Text(
+            text = "03 : 39",
+            fontSize = 32.sp,
+            modifier = Modifier.padding(top = 110.dp)
+        )
+
+        Spacer(modifier = Modifier.weight(0.8f))
+        LazyColumn(
+            modifier = Modifier,
+            verticalArrangement = Arrangement.spacedBy(6.dp)
+        ) {
+            items(pinnedAppList) {
+                Text(
+                    text = it,
+                    textAlign = TextAlign.Center,
+                    fontSize = 18.sp,
+                    modifier = Modifier
+                        .width(120.dp)
+                        .padding(0.dp, 6.dp)
+                )
+            }
+        }
+        Spacer(modifier = Modifier.weight(1f))
+
+        Text(
+            text = "03 : 39",
+            modifier = Modifier.padding(bottom = 40.dp)
+        )
+
+    }
 }
